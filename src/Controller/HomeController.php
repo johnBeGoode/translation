ad23a6bfa3226d8e8e81d3949cab2824420af73f
johnBeGoode <jonathan.martin084@gmail.com>
2024-02-07 11:11:23 +0100

    stylisation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,13 +10,14 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomeController extends AbstractController
 {
-    #[Route(path: '/{_locale}', name: 'app_home', requirements: ['_locale' => 'fr|en|it'])]
+    #[Route(path: '/{_locale}', name: 'app_home', requirements: ['_locale' => 'fr|en|it|es'])]
     // #[Route('/', name: 'app_home')
     public function index(Request $request, TranslatorInterface $translator): Response
     {   
         $firstTranslation = $translator->trans('Salut');
         $secondTranslation = $translator->trans('Ça fonctionne');
+        $thirdTranslation = $translator->trans('J\'aime Symfony');
 
-        return $this->render('home/index.html.twig', compact('firstTranslation', 'secondTranslation'));
+        return $this->render('home/index.html.twig', compact('firstTranslation', 'secondTranslation', 'thirdTranslation'));
     }
 }
