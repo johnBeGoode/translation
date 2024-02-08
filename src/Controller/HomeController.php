0bbc4ce3ff8263ae37c27fb8e68c6cf5be69fb50
johnBeGoode <jonathan.martin084@gmail.com>
2024-02-08 15:40:00 +0100

    stylisation, ajout texte à traduire

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,7 +17,8 @@ class HomeController extends AbstractController
         $firstTranslation = $translator->trans('Salut');
         $secondTranslation = $translator->trans('Ça fonctionne');
         $thirdTranslation = $translator->trans('J\'aime Symfony');
+        $fourthTranslation = $translator->trans('Alice commençait à en avoir assez d\'être assise à côté de sa soeur sur la berge, et de n\'avoir rien à faire; une fois ou deux, elle avait jeté un coup d\'oeil dans le livre que lisait sa soeur, mais il ne contenait ni images ni conversations, "et à quoi sert un livre", pensait Alice, "sans images ni conversations" ? Elle était donc en train de se demander (du mieux qu\'elle pouvait, car la chaleur de la journée la rendait très somnolente et stupide) si le plaisir de faire une chaîne de marguerites valait la peine de se lever et de cueillir les marguerites, quand soudain un lapin blanc aux yeux roses courut tout près d\'elle.');
 
-        return $this->render('home/index.html.twig', compact('firstTranslation', 'secondTranslation', 'thirdTranslation'));
+        return $this->render('home/index.html.twig', compact('firstTranslation', 'secondTranslation', 'thirdTranslation', 'fourthTranslation'));
     }
 }
